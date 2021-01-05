be34d63d852c145a4ab5f2df5ff56b2e72833863
John Olmsted <john@olmsted.io>
2021-01-04 17:36:10 -0800

    Add 'Int8' function

--- ranger.go
+++ ranger.go
@@ -20,3 +20,24 @@ func Int(start, end int) []int {
 
 	return vals
 }
+
+// Returns an interval slice of int8s, with the specified start and end value.
+// Values between the start and end are incremented ("stepped") by 1.
+//
+//   int8s := ranger.Int8(4, 8)
+//   fmt.Printf("%v", int8s) // [4 5 6 7 8]
+//
+// If the end value is smaller than the start, returns an empty slice.
+func Int8(start, end int8) []int8 {
+	if start > end {
+		return []int8{}
+	}
+
+	vals := make([]int8, (end-start)+1)
+	for i := range vals {
+		vals[i] = start
+		start++
+	}
+
+	return vals
+}
--- ranger_test.go
+++ ranger_test.go
@@ -30,3 +30,28 @@ func BenchmarkInt(b *testing.B) {
 		Int(1, 1000000)
 	}
 }
+
+func TestInt8(t *testing.T) {
+	type testVals struct {
+		start    int8
+		end      int8
+		expected []int8
+	}
+
+	tests := []testVals{
+		{1, 10, []int8{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
+		{-5, 5, []int8{-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5}},
+		{7, 7, []int8{7}},
+		{10, 1, []int8{}},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.expected, Int8(test.start, test.end))
+	}
+}
+
+func BenchmarkInt8(b *testing.B) {
+	for i := 1; i < b.N; i++ {
+		Int8(1, 127)
+	}
+}
