ac9116a2df52bfec6faf72a2fa08401b8a0de75c
John Olmsted <john@olmsted.io>
2021-01-04 17:30:39 -0800

    Add comment for 'Int'

--- ranger.go
+++ ranger.go
@@ -1,5 +1,12 @@
 package ranger
 
+// Returns an interval slice of ints, with the specified start and end value.
+// Values between the start and end are incremented ("stepped") by 1.
+//
+//   ints := ranger.Int(1, 5)
+//   fmt.Printf("%v", ints) // [1 2 3 4 5]
+//
+// If the end value is smaller than the start, returns an empty slice.
 func Int(start, end int) []int {
 	if start > end {
 		return []int{}
